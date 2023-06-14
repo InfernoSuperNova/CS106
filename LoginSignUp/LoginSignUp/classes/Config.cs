b9ebb828499196c95be6c9bee49069aace71ad97
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-06-14 16:56:09 +1200

    Creation of config file
    A file to store configuration values for arbitrary numbers, among anything else that might need to be changed with relative ease.

---
+++ LoginSignUp/LoginSignUp/classes/Config.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginSignUp.classes
+{
+    static class Config
+    {
+        public static readonly char[] DisallowedCharacters = { ',' };
+
+        public const int MaxChars = 32;
+        public const int MinChars = 5;
+    }
+}
