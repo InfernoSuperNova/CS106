b020ccd655688f86cf5b4da531c4fd8bffa5c000
Dylan Green <dylan.green801@gmail.com>
2023-06-15 14:35:42 +1200

    good progress on admin bug menu

--- LoginSignUp/LoginSignUp/pages/AdminMenu.xaml.cs
+++ LoginSignUp/LoginSignUp/pages/AdminMenu.xaml.cs
@@ -24,5 +24,10 @@ namespace LoginSignUp.pages
         {
             InitializeComponent();
         }
+
+        private void AdminProject_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
