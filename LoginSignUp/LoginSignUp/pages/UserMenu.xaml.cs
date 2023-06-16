05d3f1afa5ad9124df5bd23fd1536d4e341b1052
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-06-16 16:55:00 +1200

    Iterative project adding

--- LoginSignUp/LoginSignUp/pages/UserMenu.xaml.cs
+++ LoginSignUp/LoginSignUp/pages/UserMenu.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using LoginSignUp.UserControls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 using System.Windows.Controls;
+using System.Windows.Media.Animation;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -13,6 +16,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using LoginSignUp.classes;
 namespace LoginSignUp.pages
 {
     /// <summary>
@@ -20,9 +24,41 @@ namespace LoginSignUp.pages
     /// </summary>
     public partial class UserMenu : Page
     {
+        static int projectCount = 0;
+
+        public UserProject UserProject;
         public UserMenu()
         {
             InitializeComponent();
+
+            for (; projectCount < 5; projectCount++)
+            {
+                // Create your dynamic objects
+                var project = new UserProject();
+
+                // Add the dynamic object to the stack panel
+                ProjectField.Children.Add(project);
+                project.ProjectTitle.Text = "Project " + projectCount;
+            }
+        }
+
+        private void NewProjectBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var project = new UserProject();
+            ProjectField.Children.Add(project);
+            project.ProjectTitle.Text = "Project " + projectCount++;
+            //ProjectScrollField.ScrollToVerticalOffset(ProjectScrollField.ScrollableHeight);
+            DelayedActionHelper.DelayedAction(DispatcherPriority.Background, TimeSpan.FromMilliseconds(0), () =>
+            {
+                ProjectScrollField.ScrollToVerticalOffset(ProjectScrollField.ScrollableHeight);
+
+
+            });
+        }
+
+        private void SignOutBtn_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
