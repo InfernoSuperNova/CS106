36436c64514058ee9d90e3ef2fcda7a78fc0875d
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-06-26 19:35:42 +1200

    Revert "Remove gitignored files"
    This reverts commit 4fb734304b404dd233aa1b40150712ed85002d87.

---
+++ LoginSignUp/LoginSignUp/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+using LoginSignUp.pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LoginSignUp.classes;
+using LoginSignUp.UserControls;
+
+namespace LoginSignUp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public AccountLogin AccountLogin;
+        public AccountSignUp AccountSignUp;
+        public UserMenu UserMenu;
+        public AdminMenu AdminMenu;
+        public MainWindow()
+        {
+            AccountLogin = new AccountLogin();
+            AccountSignUp = new AccountSignUp();
+            UserMenu = new UserMenu();
+            AdminMenu = new AdminMenu();
+            InitializeComponent();
+
+            MainWindowFrame.Content = AccountLogin;
+
+            AccountLogin._NavigateToSignUpPageBtnClick += PageSignUp;
+            AccountLogin._SuccessfulLogin += GetUserAccountType;
+            AccountSignUp._NavigateToLoginPageBtnClick += PageLogin;
+            AccountSignUp._SuccessfulSignup += PageUserMenu;
+            AdminMenu._SignOut += PageLogin;
+            UserMenu._SignOut += PageLogin;
+
+            ProjectDataBase.InitializeDatabase();
+        }
+        public void GetUserAccountType(object sender, RoutedEventArgs e, List<string> UserNames, string UserName)
+        {
+            int index = UserDatabase.GetUserIndexByUserName(UserNames, UserName);
+            string userType = UserDatabase.GetUserTypeByIndex(index);
+            switch (userType)
+            {
+                case "user":
+                    PageUserMenu(sender, e);
+                    UserMenu.header.UserText.Text = UserName + " : User";
+                    break;
+                case "admin":
+                    PageAdminMenu(sender, e);
+                    AdminMenu.header.UserText.Text = UserName + " : Admin";
+                    break;
+                case "dev":
+                    PageUserMenu(sender, e);
+                    UserMenu.header.UserText.Text = UserName + " : Dev";
+                    break;
+            }
+        }
+        public void PageSignUp(object sender, RoutedEventArgs e)
+        {
+            MainWindowFrame.Content = AccountSignUp;
+        }
+        public void PageLogin(object sender, RoutedEventArgs e)
+        {
+            MainWindowFrame.Content = AccountLogin;
+        }
+        public void PageUserMenu(object sender, RoutedEventArgs e)
+        {
+            MainWindowFrame.Content = UserMenu;
+        }
+        public void PageAdminMenu(object sender, RoutedEventArgs e)
+        {
+            MainWindowFrame.Content = AdminMenu;
+        }
+    }
+}
