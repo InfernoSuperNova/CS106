82fba6ae4e0112614fefe8228eff2b0f63da9002
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-06-27 22:54:26 +1200

    Redesigning AddBug page

--- LoginSignUp/LoginSignUp/UserControls/AddBug.xaml.cs
+++ LoginSignUp/LoginSignUp/UserControls/AddBug.xaml.cs
@@ -32,11 +32,11 @@ namespace LoginSignUp.UserControls
         private void AddBtn_Click(object sender, RoutedEventArgs e)
         {
             ProjectDataBase.Bugs.Bug bug = new ProjectDataBase.Bugs.Bug();
-            bug.name = bugName.Text;
-            bug.priority = bugPriority.Text;
-            bug.timeSpent = bugTimeSpent.Text;
-            bug.description = bugDescription.Text;
-            bug.stepsToReproduce = bugStepsToReproduce.Text;
+            bug.name = BugName.Text;
+            bug.priority = BugPriority.Text;
+            bug.timeSpent = BugTimeSpent.Text;
+            bug.description = BugDescription.Text;
+            bug.stepsToReproduce = BugStepsToReproduce.Text;
             ClearFields();
             _AddBugBtn(sender, e, bug, currentProject);
             
@@ -45,11 +45,11 @@ namespace LoginSignUp.UserControls
         //There's probably a better way to do this. But this is perfectly readable and performant, so it's good enough for me
         private void ClearFields()
         {
-            bugName.Text = null;
-            bugPriority.Text = null;
-            bugTimeSpent.Text = null;
-            bugDescription.Text = null;
-            bugStepsToReproduce.Text = null;
+            BugName.Text = null;
+            BugPriority.Text = null;
+            BugTimeSpent.Text = null;
+            BugDescription.Text = null;
+            BugStepsToReproduce.Text = null;
         }
 
         internal void Enable(string projName)
@@ -62,6 +62,46 @@ namespace LoginSignUp.UserControls
         {
             Visibility = Visibility.Hidden;
             currentProject = "";
+        }
+
+        private void BugName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(BugName.Text))
+                BugNameWatermark.Visibility = Visibility.Visible;
+            else
+                BugNameWatermark.Visibility = Visibility.Hidden;
+        }
+
+        private void BugPriority_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(BugPriority.Text))
+                BugPriorityWatermark.Visibility = Visibility.Visible;
+            else
+                BugPriorityWatermark.Visibility = Visibility.Hidden;
+        }
+
+        private void BugTimeSpent_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(BugTimeSpent.Text))
+                BugTimeSpentWatermark.Visibility = Visibility.Visible;
+            else
+                BugTimeSpentWatermark.Visibility = Visibility.Hidden;
+        }
+
+        private void BugDescription_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(BugDescription.Text))
+                BugDescriptionWatermark.Visibility = Visibility.Visible;
+            else
+                BugDescriptionWatermark.Visibility = Visibility.Hidden;
+        }
+
+        private void BugStepsToReproduce_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(BugStepsToReproduce.Text))
+                BugStepsToReproduceWatermark.Visibility = Visibility.Visible;
+            else
+                BugStepsToReproduceWatermark.Visibility = Visibility.Hidden;
         }
     }
 }
